17b0f34eecff32ff8c3dbfdff54ee34c4a072905
Orestis <orestispanago@gmail.com>
2022-10-26 14:46:52 +0300

    Using recursive function. Getting all file paths as list, then selecting

--- main.py
+++ main.py
@@ -5,40 +5,32 @@ FTP_USER = ""
 FTP_PASS = ""
 FTP_DIR = "/christina/Mobotix/2022"
 
-
+all_paths = []
 csv_files = []
 img_files = []
 
-with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
-    ftp.cwd(FTP_DIR)
-    months = ftp.nlst()[2:]
-    for month in months:
-        ftp.cwd(f"{FTP_DIR}/{month}")
-        days = ftp.nlst()[2:]
-        for day in days:
-            ftp.cwd(f"{FTP_DIR}/{month}/{day}")
-            hours = ftp.nlst()[2:]
-            for hour in hours:
-                ftp.cwd(f"{FTP_DIR}/{month}/{day}/{hour}")
-                minutes = ftp.nlst()[2:]
-                for minute in minutes:
-                    minute_path = f"{FTP_DIR}/{month}/{day}/{hour}/{minute}"
-                    ftp.cwd(minute_path)
-                    files = ftp.nlst()[2:]
-                    if "x90_y90.csv" in files:
-                       csv_files.append(f"{minute_path}/x90_y90.csv")
-                    if "x90_y90.jpg" in files:
-                        img_files.append(f"{minute_path}/x90_y90.jpg")
-                    if "x90_y90.png" in files:
-                        img_files.append(f"{minute_path}/x90_y90.png")
+def list_recursive(ftp, remotedir):
+    ftp.cwd(remotedir)
+    for entry in ftp.mlsd():
+        remotepath = remotedir + "/" + entry[0]
+        if entry[1]['type'] == 'dir':
+            list_recursive(ftp, remotepath)
+        elif entry[1]['type'] == 'file':
+            all_paths.append(remotepath)
+            
 
-                
-# common = list(set(csv_files).intersection(jpg_files))
-# uncommon = list(set(csv_files).symmetric_difference(jpg_files))
+with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
+    list_recursive(ftp, FTP_DIR)
+
+for f in all_paths:
+    if "x90_y90.csv" in f:
+        csv_files.append(f)
+    elif "x90_y90.jpg"in f or "x90_y90.png" in f:
+        img_files.append(f)
 
 with open('csv_files.txt', 'w') as f:
     for line in csv_files:
-        f.write(f"{line}\n")
+        f.write(line + "\n")
 with open('img_files.txt', 'w') as f:
     for line in img_files:
-        f.write(f"{line}\n")
+        f.write(line + "\n")
