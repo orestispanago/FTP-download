69050caa3f37680092a4da65a2f8294b19385929
orestispanago <orestispanago@gmail.com>
2022-11-02 18:19:40 +0200

    Implemented pattern based selection

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FTP-list
+
+Gets list of remote file paths in FTP directory recursively.
+
+Pattern based selection (glob-like) is available with ```fnmatch```
--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 from ftplib import FTP
+import fnmatch
+
 
 FTP_IP = ""
 FTP_USER = ""
 FTP_PASS = ""
 FTP_DIR = "/christina/Mobotix/2022"
 
-all_paths = []
-csv_files = []
-img_files = []
 
-def list_recursive(ftp, remotedir):
+def list_recursive(ftp, remotedir, file_paths):
     ftp.cwd(remotedir)
     for entry in ftp.mlsd():
         remotepath = remotedir + "/" + entry[0]
-        if entry[1]['type'] == 'dir':
-            list_recursive(ftp, remotepath)
-        elif entry[1]['type'] == 'file':
-            all_paths.append(remotepath)
-            
+        if entry[1]["type"] == "dir":
+            list_recursive(ftp, remotepath, file_paths)
+        elif entry[1]["type"] == "file":
+            file_paths.append(remotepath)
 
-with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
-    list_recursive(ftp, FTP_DIR)
 
-for f in all_paths:
-    if "x90_y90.csv" in f:
-        csv_files.append(f)
-    elif "x90_y90.jpg"in f or "x90_y90.png" in f:
-        img_files.append(f)
+def get_remote_paths(ip=FTP_IP, user=FTP_USER, passwd=FTP_PASS, folder=FTP_DIR):
+    all_paths = []
+    with FTP(ip, user, passwd) as ftp:
+        list_recursive(ftp, folder, all_paths)
+    return all_paths
 
-with open('csv_files.txt', 'w') as f:
+
+remote_paths = get_remote_paths()
+
+csv_files = fnmatch.filter(remote_paths, "*x90_y90.csv")
+img_files = fnmatch.filter(remote_paths, "*x90_y90.*g")
+
+with open("csv_files.txt", "w") as f:
     for line in csv_files:
         f.write(line + "\n")
-with open('img_files.txt', 'w') as f:
+with open("img_files.txt", "w") as f:
     for line in img_files:
         f.write(line + "\n")
