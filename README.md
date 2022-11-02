422f85e1e8a0e6a964d9acee4f0cfc3a28bff10c
orestispanago <orestispanago@gmail.com>
2022-11-02 19:50:46 +0200

    Added download functionality

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Gets list of remote file paths in FTP directory recursively.
 
 Pattern based selection (glob-like) is available with ```fnmatch```
+
+Downloads list of remote files to local folder (flattened path).
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import fnmatch
 FTP_IP = ""
 FTP_USER = ""
 FTP_PASS = ""
-FTP_DIR = "/christina/Mobotix/2022"
+FTP_DIR = "/christina/Mobotix/2022/11/02"
 
 
 def list_recursive(ftp, remotedir, file_paths):
@@ -13,16 +13,27 @@ def list_recursive(ftp, remotedir, file_paths):
     for entry in ftp.mlsd():
         remotepath = remotedir + "/" + entry[0]
         if entry[1]["type"] == "dir":
+            print("Found FTP dir:", remotepath)
             list_recursive(ftp, remotepath, file_paths)
         elif entry[1]["type"] == "file":
             file_paths.append(remotepath)
 
 
-def get_remote_paths(ip=FTP_IP, user=FTP_USER, passwd=FTP_PASS, folder=FTP_DIR):
+def get_remote_paths():
     all_paths = []
-    with FTP(ip, user, passwd) as ftp:
-        list_recursive(ftp, folder, all_paths)
+    with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
+        list_recursive(ftp, FTP_DIR, all_paths)
     return all_paths
+
+
+def download(remote_files, local_folder):
+    with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
+        for remote_path in remote_files:
+            flat_path = remote_path.replace("/", "")
+            local_path = f"{local_folder}/{flat_path}"
+            with open(local_path, 'wb' ) as f :
+                ftp.retrbinary('RETR ' + remote_path, f.write)
+            print("Downloaded file:", local_path)
 
 
 remote_paths = get_remote_paths()
@@ -30,9 +41,4 @@ remote_paths = get_remote_paths()
 csv_files = fnmatch.filter(remote_paths, "*x90_y90.csv")
 img_files = fnmatch.filter(remote_paths, "*x90_y90.*g")
 
-with open("csv_files.txt", "w") as f:
-    for line in csv_files:
-        f.write(line + "\n")
-with open("img_files.txt", "w") as f:
-    for line in img_files:
-        f.write(line + "\n")
+download(csv_files, "downloads")
