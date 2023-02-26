abd627b8d413d591f4ae2277e36a932c990579a7
orestispanago <orestispanago@gmail.com>
2023-02-26 19:37:36 +0200

    Added renamer (adding prefix)

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Gets list of remote file paths in FTP directory recursively.
 Selects files based on pattern (glob-like).
 
 Downloads list of remote files to local folder (flattened path).
+
+Renames remote files (adding prefix). Renaming requires FTP write permissions.
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from ftplib import FTP
 import fnmatch
-
+from ftplib import FTP
 
 FTP_IP = ""
 FTP_USER = ""
 FTP_PASS = ""
-FTP_DIR = "/christina/Mobotix/2022/11/02"
+FTP_DIR = "/dataloggers/rsi"
 
 
 def list_recursive(ftp, remotedir, file_paths):
@@ -31,14 +30,27 @@ def download(remote_files, local_folder):
         for remote_path in remote_files:
             flat_path = remote_path.replace("/", "")
             local_path = f"{local_folder}/{flat_path}"
-            with open(local_path, 'wb' ) as f :
-                ftp.retrbinary('RETR ' + remote_path, f.write)
+            with open(local_path, "wb") as f:
+                ftp.retrbinary("RETR " + remote_path, f.write)
             print("Downloaded file:", local_path)
 
 
+def rename(remote_paths, prefix=""):
+    with FTP(FTP_IP, FTP_USER, FTP_PASS) as ftp:
+        for src_path in remote_paths:
+            base_name = src_path.split("/")[-1]
+            if not base_name.startswith(prefix):
+                dest_path = src_path.replace(base_name, f"{prefix}{base_name}")
+                ftp.rename(src_path, dest_path)
+                print(f"Renamed {src_path} to {dest_path}")
+
+
+# Example usage
 remote_paths = get_remote_paths()
 
 csv_files = fnmatch.filter(remote_paths, "*x90_y90.csv")
 img_files = fnmatch.filter(remote_paths, "*x90_y90.*g")
 
 download(csv_files, "downloads")
+
+# rename(remote_paths, prefix="rsi1min_")
